9eb45ac6eb65f6812a9e91a0592b0a77ecf7d7cb
cjllove <cjllove@live.cn>
2018-02-13 20:29:29 +0800

    Initial commit

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\Request;
+use think\Db;
+class Index extends Controller
+{
+    public function index($name='')
+    {
+        return 'zz'.$name;
+    }
+}
+?>
---
+++ application/index/controller/Index2.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+use think\Controller;
+class Index2 extends controller{
+   function geta (){
+    print_r('jjfjj');
+
+   }
+}
+?>
