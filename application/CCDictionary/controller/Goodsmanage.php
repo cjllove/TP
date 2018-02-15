66eb89c96a01d3bc0e6e5438f50b7c41e0ea063f
cjllove <cjllove@live.cn>
2018-02-15 14:20:31 +0800

    1

---
+++ application/CCDictionary/controller/Goodsmanage.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\CCDictionary\controller;
+use think\Controller;
+use think\Db;
+use think\Request;
+class Goodsmanage extends controller{
+    function index (){
+        print_r('wtf?');
+    }
+}
+?>
